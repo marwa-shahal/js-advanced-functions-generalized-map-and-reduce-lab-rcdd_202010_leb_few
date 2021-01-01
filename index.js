a161dff77ef9a9bd8387ea55a9463446fb185106
Marwa Sh <marwa.al.shahal@gmail.com>
2021-01-01 18:40:17 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Add your functions here
+
+
+function map(source,func) {
+  let r = []
+  for (let i = 0; i < source.length; i++ ) {
+    r.push(func(source[i]))
+  }
+  return r
+}
+
+
+function reduce(array, combine, start) {
+  let current = (!!start)? start:array[0];
+  let i= (!!start)? 0:1;
+  for (i;i<array.length;i++) {
+    current = combine(current, array[i]);
+  }
+  return current;
+}
+  
+
