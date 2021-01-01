6f9e37722e5878fbfd49c946d7e4cc5160a295f6
Marwa Sh <marwa.al.shahal@gmail.com>
2021-01-01 16:41:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Add your functions here
+
+
+function map(source,func) {
+  let r = []
+  for (let i = 0; i < source.length; i++ ) {
+    let sr=source[i]
+    r.push(func(sr))
+  }
+  return r
+}
+
+
+function reduce(s,acc){
+  if()
+  ()
+}
+
