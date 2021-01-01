14f853388126253e0e292b15e09ce806bf41a1b3
Marwa Sh <marwa.al.shahal@gmail.com>
2021-01-01 17:50:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,20 +4,18 @@
 function map(source,func) {
   let r = []
   for (let i = 0; i < source.length; i++ ) {
-    let sr=source[i]
-    r.push(func(sr))
+    r.push(func(source[i]))
   }
   return r
 }
 
 
-function reduce(arr,acc,starter){
-  if (arr.length===0){
-    return starter
-  }
-  else {
-    
-  }
+function reduce(arr,callB,starter){
+  let start= (starter)? starter:arr[0];
+  for (let i=0; i<arr.length;i++){
+  start=callB(arr[i],start)
+}
+  return start
 }
   
 
